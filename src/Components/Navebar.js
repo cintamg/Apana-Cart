c0251891cbc644d8ff14512d63612f699b914778
cintamg <76482225+cintamg@users.noreply.github.com>
2023-12-30 12:56:00 -0800

    initial commit

---
+++ src/Components/Navebar.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Navebar extends Component {
+    render() { 
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Apana Cart</a>
+            </div>
+            </nav>
+        );
+    }
+}
+ 
+export default Navebar;
