a37f249afb64a141ce63f6063dc105639ada6376
cintamg <76482225+cintamg@users.noreply.github.com>
2023-12-30 15:33:46 -0800

    add product

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import Navebar from './Components/Navebar';
+import ProductList from './Components/ProductList';
+import Footer from './Components/Footer';
 
 function App() {
+  const productList = [
+    {
+      price : 7000,
+      name : "Redmi 7A",
+      quantity : 0
+    },
+    {
+      price : 20000,
+      name : "Moto Note 6",
+      quantity : 0
+    }
+  ];
   return (
     <>
     <Navebar/>
+    <main className='container mt-5'>
+    <ProductList productList={productList}/>
+    </main>
+    <Footer/>
     </>
   );
 }
